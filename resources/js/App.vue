b6b1e72a4fd8a358832e6c5fcac6deaa6baf1a4a
alexander z <misterobot404@gmail.com>
2023-03-30 11:16:57 +1000

    Структура проекта DONE

---
+++ resources/js/App.vue
@@ -0,0 +1,25 @@
+<template>
+    <Layout>
+        <router-view/>
+    </Layout>
+</template>
+
+<script>
+/*
+import Vue from "vue"
+import Vuesax from 'vuesax'
+import 'vuesax/dist/vuesax.css'
+Vue.use(Vuesax);
+*/
+
+import Layout from "@/components/Layout.vue";
+
+export default {
+    name: "App",
+    components: {Layout}
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/pages/Home.vue
@@ -0,0 +1,13 @@
+<template>
+    Это мой дом!
+</template>
+
+<script>
+export default {
+    name: "Home"
+}
+</script>
+
+<style scoped>
+
+</style>
