cf30ef4e751f362b6e8b36941466ebe1fd9199a9
Alexander Dmitriev <alex4056546@yandex.ru>
2023-05-24 15:07:09 +1000

    Create helps template

--- resources/js/pages/Helps.vue
+++ resources/js/pages/Helps.vue
@@ -1,15 +1,36 @@
 <template>
-    <div>
-        Помощь
+    <div class="q-px-lg q-py-md">
+        <q-timeline color="secondary">
+            <q-timeline-entry heading body="Ответы на часто задаваемые вопросы" />
+
+            <q-timeline-entry v-for="item in items" :title="item.title" :body="item.body" />
+
+        </q-timeline>
     </div>
 </template>
 
 <script>
 export default {
+    data() {
+        return {
+            items: [
+                { title: "Question_Title", body: "body" },
+                { title: "Question_Title", body: "body" },
+                { title: "Question_Title", body: "body" },
+                { title: "Question_Title", body: "body" },
+                { title: "Question_Title", body: "body" },
+                { title: "Question_Title", body: "body" },
+                { title: "Question_Title", body: "body" },
+                { title: "Question_Title", body: "body" },
+                { title: "Question_Title", body: "body" },
+                { title: "Question_Title", body: "body" },
+
+            ],
+
+        };
+    },
     name: "Helps"
 }
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
